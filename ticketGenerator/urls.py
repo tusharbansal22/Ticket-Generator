1e1fb5af89b782ef90853c39abd71d98533c9b35
tusharb <tusharbansal@iitbhilai.ac.in>
2023-01-05 12:05:01 +0530

    added ticket generator

--- ticketGenerator/urls.py
+++ ticketGenerator/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
-    path('ticket/',views.ticket)
+    path('ticket',views.ticket)
 ]
--- ticketGenerator/views.py
+++ ticketGenerator/views.py
@@ -9,5 +9,9 @@ def home(request):
 def ticket(request):
   return render(request,'ticket.html',
   {'name':request.POST['name'],
+  "coach":request.POST['coach'],
   'seat_num':request.POST['seat-number'],
-  'date':request.POST['date']})
+  'date':request.POST['date'],
+  'time':request.POST['time'],
+  'start_station':request.POST['start-station'],
+  'end_station':request.POST['leaving-station']})
